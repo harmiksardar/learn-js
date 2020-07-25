c9046eb749f0dbdd3c5e1fc4b8976393fc258808
harmiksardar <harmik.sardar@gmail.com>
2020-07-25 16:20:32 +1000

    Added another test to the test-suite

---
+++ test/sample.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+function area(l, w) {
+ if (l <= 0) {
+  return 'undefined';
+ } else if (w <= 0) {
+  return 'undefined';
+ } else {
+  return l * w;
+ }
+}
+describe('Area', function() {
+ it('should return 18 when length is 6 and width is 3', function() {
+  assert.equal(area(6, 3), 18);
+ })
+ it('should return 6 when length is 3 and width is 2', function() {
+  assert.equal(area(3, 2), 6);
+ });
+});
