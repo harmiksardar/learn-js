c97571ea1cece6145365c594fd48beb2e955e0f2
harmiksardar <harmik.sardar@gmail.com>
2020-07-28 17:36:24 +1000

    clean up the project

--- test/mocha-only.js
+++ test/mocha-only.js
@@ -10,7 +10,7 @@ function area(l, w) {
  }
 }
 
-describe('Calculate Area', function() {
+describe('Calculating Area', function() {
  it('should return 18 when length is 6 and width is 3', function() {
   assert.equal(area(6, 3), 18);
  })
